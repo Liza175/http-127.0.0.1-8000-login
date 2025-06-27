f872b6d6ccf0672619ca3bfc08b60c0bdff553b3
Liza175 <liliyulizayuliza@gmail.com>
2025-06-27 22:37:29 +0700

    fix composer.json and add start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Jalankan migrasi (opsional, kalau pakai DB)
-php artisan migrate --force
-
-# Jalankan Laravel pakai PHP built-in server
-php artisan serve --host=0.0.0.0 --port=$PORT
